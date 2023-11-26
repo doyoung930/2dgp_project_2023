6ba09ba0986edbd3ea3ecdb505e1b681d8281d6c
doyoung930 <doyoung930@gmail.com>
2023-11-26 17:39:21 +0900

    쓸데없는 코드 삭제

--- code/character_obj.py
+++ code/character_obj.py
@@ -28,17 +28,3 @@ class Exp:
     def update(self):
         self.w = self.exp/100*1240
         pass
-
-class HP:
-    image = None
-    def __init__(self, c_hp, c_x, c_y):
-        if HP.image == None:
-            HP.image = load_image('./png/gui/hp_gauge2.png')
-        self.c_hp, self.x, self.y = 0, c_x, c_y+5
-        self.w, self.h = 0, 40
-
-    def draw(self):
-        self.image.composite_draw(0, ' ', self.x, self.y, 100, 10)
-
-    def update(self):
-        pass
